2bfb9479112c8b1afbb06e0f7bafc859846c0b0d
Alan Yang <alan@ubuntu.(none)>
2011-10-09 18:38:16 -0700

    Finished teh password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,41 @@ class User < ActiveRecord::Base
 						 #The line above both creates a password_confirmation attribute
 						 #and makes sure that it matches the password attribute...somehow
  					     :length => { :within => 6..40 }
+ 
+    #register callback method encrypt_password
+    before_save :encrypt_password
+
+    def has_password?(submitted_password)
+       encrypted_password == encrypt(submitted_password)
+    end
+
+    #Class Method
+    def self.authenticate(email, submitted_password)
+       user = find_by_email(email) #Can omit 'User'.find_by_email since we are in the User class already
+       return nil if user.nil?
+       return user if user.has_password?(submitted_password)
+    end
+
+    #private method
+    private 
+      
+      def encrypt_password
+      	#ActiveRecord boolean new_record?. Will only run salt if its a new record
+        self.salt = make_salt if new_record? 
+      	self.encrypted_password = encrypt(password)
+      end
+
+      def encrypt(string)
+      	secure_hash("#{salt}--#{string}")
+      end
+
+      def secure_hash(string)
+        Digest::SHA2.hexdigest(string)
+      end
+
+      def make_salt
+      	secure_hash("#{Time.now.utc}--{password}")
+      end
 end
 
 
