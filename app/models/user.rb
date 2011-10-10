a8362ea27c4cfe9fadd5cf41a3922c72bbc34160
Alan Yang <alan@ubuntu.(none)>
2011-10-09 17:12:33 -0700

    Added password and encrypted_password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor   :password
+	attr_accessible :name, :email, :password, :password_confirmation
+	#put anything that is received from the web in attr_accessible
     
     email_regex = /\A[\w+.\-]+@[a-z.\d\-]+\.[a-z]+\z/i
 
@@ -9,16 +11,24 @@ class User < ActiveRecord::Base
 	validates :email, :presence => true, 
 	                  :format => { :with => email_regex },
 	                  :uniqueness => { :case_sensitive => false }
+
+	validates :password, :presence => true,
+						 :confirmation => true,
+						 #The line above both creates a password_confirmation attribute
+						 #and makes sure that it matches the password attribute...somehow
+ 					     :length => { :within => 6..40 }
 end
+
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,111 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+   
+   before(:each) do
+   	 @attr = {:name => "Example User", 
+	   	      :email => "user@example.com",
+	   	      :password => "foobar",
+	   	      :password_confirmation => "foobar"
+	   	     }
+   end
+
+   it "should create a new instance given a valid attribute hash" do
+   	 User.create!(@attr)
+   end
+
+   it "should require a name" do
+   	 no_name_user = User.new(@attr.merge(:name => ""))
+   	 no_name_user.should_not be_valid
+   end
+
+   it "should require an email address" do
+   	 no_email_user = User.new(@attr.merge( :email => ""))
+   	 no_email_user.should_not be_valid
+   end
+
+   it "should reject names that are too long" do
+   	 long_name = 'a' * 51
+   	 long_name_user = User.new(@attr.merge( :name => long_name))
+   	 long_name_user.should_not be_valid
+   end
+
+   it "should accept valid email addresses" do
+   	 addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+   	 addresses.each do |addr|
+   	 	valid_email_user = User.new(@attr.merge(:email => addr))
+   	 	valid_email_user.should be_valid
+   	 end
+   end
+
+   it "should not accept invalid email addresses" do
+   	 addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+   	 addresses.each do |addr|
+   	 	invalid_email_user = User.new(@attr.merge(:email => addr))
+   	 	invalid_email_user.should_not be_valid
+   	 end
+   end
+
+   describe "passwords" do
+
+	   before(:each) do
+	    	@user = User.new(@attr)
+	   end
+
+	   it "should have a password attribute" do
+		    @user.should respond_to(:password)
+	   end
+     
+       it "should have a password confirmation attribute" do
+    	   @user.should respond_to(:password_confirmation)
+       end
+    end
+
+    describe "password validation" do
+       it "should require a password" do
+       	User.new(@attr.merge(:password => "", :password_confirmation => "")).
+       	  should_not be_valid
+        end
+
+       it "should require a matching password confirmation" do
+       	User.new(@attr.merge(:password_confirmation => "invalid")).
+       	  should_not be_valid
+       end
+
+       it "should reject short passwords" do
+       	 short = "a" * 5
+       	 hash = @attr.merge(:password => "short", :password_confirmation => "short")
+         User.new(hash).should_not be_valid
+       end
+
+       it "should reject long passwords" do
+       	 long = "a" * 41
+       	 hash = @attr.merge(:password => "long", :password_confirmation => "long")
+         User.new(hash).should_not be_valid
+       end
+   end  
+
+   describe "password encryption" do
+
+   	 before(:each) do
+   	   @user = User.create!(@attr)
+   	 end
+
+   	 it "should have an encrypted password attribute" do
+   	   @user.should respond_to(:encrypted_password)
+     end
+   end
 end
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
